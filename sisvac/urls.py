42b87441230818de9dbe8d5d3362287429167670
marcosmn <marcosmnobrega@gmail.com>
2020-12-18 19:43:30 -0300

    Definir URL da api para /api/v1

--- sisvac/urls.py
+++ sisvac/urls.py
@@ -34,6 +34,6 @@ urlpatterns = [
     #path('', include('vacinacao.urls')),
     #path('', TemplateView.as_view(template_name='index.html')),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    #path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api/v1/', include(router.urls)),
+    #path('api/v1/', include('rest_framework.urls', namespace='rest_framework'))
 ]
