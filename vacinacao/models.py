c1371395a91464f704cfcacb3c66d8077429f35b
marcosmn <marcosmnobrega@gmail.com>
2020-12-18 20:10:38 -0300

    Model para vacinacao

--- vacinacao/models.py
+++ vacinacao/models.py
@@ -15,12 +15,18 @@ class LoteVacina(models.Model):
     quantidade_estoque = models.IntegerField()
     vacina = models.ForeignKey(Vacina, on_delete=models.PROTECT)
 
+class Estabelecimento(models.Model):
+    co_unidade = models.CharField(max_length=15)
+
+
 class Vacinacao(models.Model):
     vacinacao_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     data_vacinacao = models.DateField()
     data_aprovado = models.DateField()
     data_agendamento = models.DateField()
     data_solicitacao = models.DateField()
+    estabelecimento = models.ForeignKey(Estabelecimento, on_delete=models.PROTECT)
+    user_id = models.CharField(max_length=32)
     privada = models.BooleanField()
     vacinado = models.BooleanField()
     vacina = models.ForeignKey(Vacina, on_delete=models.PROTECT)
