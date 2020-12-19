55c53828b94de98de6c57b38d175430ad0c5daf8
marcosmn <marcosmnobrega@gmail.com>
2020-12-18 23:27:25 -0300

    Incluir modelos em Admin

--- sisvac/settings.py
+++ sisvac/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     'social_django',
     'rest_framework_social_oauth2',
     'oauth2_provider',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -87,6 +89,12 @@ DATABASES = {
     }
 }
 
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8080',
+    '127.0.0.1:8080'
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
--- vacinacao/admin.py
+++ vacinacao/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Vacina, LoteVacina, Municipio, Estabelecimento, Vacinacao
+
+admin.site.register(Vacina)
+admin.site.register(LoteVacina)
+admin.site.register(Municipio)
+admin.site.register(Estabelecimento)
+admin.site.register(Vacinacao)
