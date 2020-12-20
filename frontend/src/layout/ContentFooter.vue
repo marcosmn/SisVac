37f0d0513e848f018cc5fa44f25cdb84c63d3970
marcosmn <marcosmnobrega@gmail.com>
2020-12-20 20:52:17 -0300

    home e css footer

--- frontend/src/layout/ContentFooter.vue
+++ frontend/src/layout/ContentFooter.vue
@@ -1,11 +1,10 @@
 <template>
   <footer class="footer">
     <div class="container-fluid">
-     
       <div class="copyright text-center">
         &copy; SUS - SisVac
         by
-        <a href="https://www.linkedin.com/in/marcos-martins-n%C3%B3brega-606824119/" target="_blank">Marcos Martins  Nóbrega</a> /
+         <a href="https://www.linkedin.com/in/marcos-martins-n%C3%B3brega-606824119/" target="_blank">Marcos Martins  Nóbrega</a> /
          <a href="https://www.linkedin.com/in/marcos-martins-n%C3%B3brega-606824119/" target="_blank">Curriculo Lattes</a>.
       </div>
     </div>
@@ -15,6 +14,12 @@
   export default {}
 
 </script>
-<style>
+<style scoped>
+  a{
+    color: #0D3571;
+}
 
+div.copyrigh{
+  text-align: center;
+}
 </style>
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="login">
+  <div class="home">
     <div class="panel panel-default">
       <div class="panel-body">
         <div class="text-center">
@@ -7,22 +7,15 @@
             <div class="col">
               <h2>Carteira digital de vacinação</h2>
               <h3>O que é?</h3>
-              <p>
-                A carteira digital de vacinação é uma plataforma online para os
-                usuários do <b>Sistema Único de Saúde.</b>
-              </p>
-              <p>
-                Nela você pode <b>visualizar</b> seu
-                <b>cartão de vacinação, realizar agendamento de vacinas</b>
-                podendo escolher <b>local, data e hora</b> que deseja ser
-                vacinado.
-              </p>
+              <p>A carteira digital de vacinação é uma plataforma online para os usuários do <b>Sistema Único de Saúde.</b></p>
+              <p>Nela você pode <b>visualizar</b> seu <b>cartão de vacinação, realizar agendamento de vacinas</b>
+                podendo escolher <b>local, data e hora</b> que deseja ser vacinado.</p>
+
               <h3>O que mais posso fazer?</h3>
               <p><b>Visualizar seu Cartão de Vacina</b></p>
               <p><b>Consultar sua posição na fila de espera da vacina</b></p>
-              <p>
-                <b>Registrar a aplicação de vacinas de clínicas particulares</b>
-              </p>
+              <p> <b>Registrar na carteira de vacina, aplicação de vacinas tomadas em clínicas particulares</b></p>
+              
             </div>
           </div>
         </div>
@@ -41,9 +34,6 @@ export default {
 </script>
 
 <style scoped>
-button.btn-google {
-  background-color: white;
-}
 
 h2 {
   color: rgba(13, 53, 113, 1);
@@ -57,5 +47,9 @@ h3 {
 p {
   text-align: left;
 }
+.home{
+  margin-bottom: 112px;
+}
+
 
 </style>
