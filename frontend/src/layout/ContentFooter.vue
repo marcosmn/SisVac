b90e764d1c6387ac22d599e70a9907c608328c16
marcosmn <marcosmnobrega@gmail.com>
2020-12-20 23:55:22 -0300

    login

--- frontend/src/layout/ContentFooter.vue
+++ frontend/src/layout/ContentFooter.vue
@@ -2,10 +2,10 @@
   <footer class="footer">
     <div class="container-fluid">
       <div class="copyright text-center">
-        &copy; SUS - SisVac
-        by
+        &copy; SUS - SisVac : 
+        
          <a href="https://www.linkedin.com/in/marcos-martins-n%C3%B3brega-606824119/" target="_blank">Marcos Martins  Nóbrega</a> /
-         <a href="https://www.linkedin.com/in/marcos-martins-n%C3%B3brega-606824119/" target="_blank">Curriculo Lattes</a>.
+         <a href="https://www.linkedin.com/in/marcos-martins-n%C3%B3brega-606824119/" target="_blank">Curriculo Lattes</a>
       </div>
     </div>
   </footer>
--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -6,13 +6,16 @@
           <div class="container">
             <div class="row">
               <div class="col">
-                <div class="colorfundo">
-                  <h2>Acesso a conta</h2>
+                  <div class="imgtopologin">
+                      <img class="avatar border-gray" src="img/logo.png" alt="Logo"/>
+                    <h2>Login</h2>
+                    <p>Área de acesso restrito</p>
+                  </div>
                     <a class="btn btn-lg btn-google" href="/auth/login/google-oauth2">
-                      <i class="fa fa-google fa-1x"></i> Login com o Google
+                    <img width="30px" style="margin-bottom:3px; margin-right:5px" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"/>
+                     Login com o Google
                     </a>
                 </div>
-              </div>
             </div>
           </div>
         </div>
@@ -41,23 +44,16 @@ export default {
 </script>
 
 <style scoped>
+  .login {
+    height:503px;
+  }
+   
+  .imgtopologin {
+    padding-top:30px;
+  }
+
   button.btn-google {
     background-color : white;
   }
-
-  h2{
- 
-  color : rgba(13, 53, 113, 1);
-  }
-
-  h3{
-  text-align:left;
-  color : rgba(13, 53, 113, 1);
-  }
-
-  p{
-    text-align:left;
-  }
-
-  
+    
 </style>
